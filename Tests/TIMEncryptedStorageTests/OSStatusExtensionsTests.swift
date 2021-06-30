7120d4a1f62d5d48c8c2f136c362ebc26a9d8b38
Thomas Kalhøj Clemensen <tkc@trifork.com>
2021-06-30 11:29:59 +0200

    Fixed failing unit tests.

--- Tests/TIMEncryptedStorageTests/OSStatusExtensionsTests.swift
+++ Tests/TIMEncryptedStorageTests/OSStatusExtensionsTests.swift
@@ -5,7 +5,11 @@ import Security
 final class OSStatusExtensionsTests: XCTestCase {
     func testErrorDescription() {
         let status: OSStatus = errSecCreateChainFailed
-        let expectedString = "[-25318]: A required component (data storage module) could not be loaded. You may need to restart your computer."
+        let expectedString = "[-25318]: One or more certificates required to validate this certificate cannot be found."
         XCTAssertEqual(expectedString, status.errorDescription)
+
+        let status2: OSStatus = errSecInteractionRequired
+        let expectedString2 = "[-25315]: User interaction is required, but is currently not allowed."
+        XCTAssertEqual(expectedString2, status2.errorDescription)
     }
 }
