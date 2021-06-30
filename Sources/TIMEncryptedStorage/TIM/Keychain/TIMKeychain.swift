8440c8b88ebb208bd3b266bdc2a09f3a0c10a284
Thomas Kalhøj Clemensen <tkc@trifork.com>
2021-06-30 14:53:58 +0200

    Updated comment which describes an OSStatus happening.

--- Sources/TIMEncryptedStorage/TIM/Keychain/TIMKeychain.swift
+++ Sources/TIMEncryptedStorage/TIM/Keychain/TIMKeychain.swift
@@ -99,7 +99,7 @@ public final class TIMKeychain : TIMSecureStorage {
     func mapLoadStatusToResult(_ status: OSStatus, data: AnyObject?) -> Result<Data, TIMSecureStorageError> {
         let result: Result<Data, TIMSecureStorageError>
         switch status {
-        // `errSecInteractionNotAllowed` can occur if the app is put into the background while the biometric dialog is open.
+        // `errSecInteractionNotAllowed` can occur if the screen is locked while the biometric dialog is open.
         case errSecAuthFailed, errSecUserCanceled, errSecInteractionNotAllowed:
             result = Result.failure(.authenticationFailedForData)
         case noErr:
