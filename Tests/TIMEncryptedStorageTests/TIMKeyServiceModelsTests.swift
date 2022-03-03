5ce388f002c9d561ba4e7554d66f2085572e8ddc
Jon Juhl Nørrelykke <jhn@trifork.com>
2022-03-03 12:53:45 +0100

    Re-enabled test - wasn't at fault

--- Tests/TIMEncryptedStorageTests/TIMKeyServiceModelsTests.swift
+++ Tests/TIMEncryptedStorageTests/TIMKeyServiceModelsTests.swift
@@ -5,16 +5,16 @@
 //  Created by Jon Nørrelykke on 03/03/2022.
 //
 
-//import XCTest
-//import TIMEncryptedStorage  // Intentional in order to test public scopes
-//
-//class TIMKeyModelsTests: XCTestCase {
-//
-//    func testTIMKeyModelConstructor() {
-//        let model = TIMKeyModel(keyId: "TheKeyID", key: "TheKey", longSecret: "TheLongSecret")
-//        XCTAssertEqual("TheKeyID", model.keyId)
-//        XCTAssertEqual("TheKey", model.key)
-//        XCTAssertEqual("TheLongSecret", model.longSecret)
-//    }
-//
-//}
+import XCTest
+import TIMEncryptedStorage  // Intentional in order to test public scopes
+
+class TIMKeyModelsTests: XCTestCase {
+
+    func testTIMKeyModelConstructor() {
+        let model = TIMKeyModel(keyId: "TheKeyID", key: "TheKey", longSecret: "TheLongSecret")
+        XCTAssertEqual("TheKeyID", model.keyId)
+        XCTAssertEqual("TheKey", model.key)
+        XCTAssertEqual("TheLongSecret", model.longSecret)
+    }
+
+}
